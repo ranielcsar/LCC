32b10cbd34e798b16d251efcff5cf6fb039855a1
ranoob <ranielcsardev@gmail.com>
2024-08-26 09:01:56 -0300

    fix: retorno da função de ver bucho

--- dev.web2/lista_atvs/8_macaco/Macaco.py
+++ dev.web2/lista_atvs/8_macaco/Macaco.py
@@ -7,12 +7,13 @@ class Macaco:
         print(f"o macaco {self.__nome} comeu {comida}")
         self.__bucho.append(comida)
 
-    def ver_bucho(self) -> None:
+    def ver_bucho(self) -> str:
         text = ''
         for comida in self.__bucho:
             text += f"{comida}, "
         
         print(f"comidas no bucho: [{text[:-2]}]")
+        return f"[{text[:-2]}]"
 
     def digerir(self) -> None:
         print("toda comida foi digerida!")
