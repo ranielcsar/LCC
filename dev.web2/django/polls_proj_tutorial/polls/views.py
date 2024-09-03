fe1ec47bab9d4b34fbe64d3f7a1be3ad47191719
ranoob <ranielcsardev@gmail.com>
2024-09-03 09:43:00 -0300

    feat: passando dados para template e recebendo parametro via request

--- dev.web2/django/polls_proj_tutorial/polls/views.py
+++ dev.web2/django/polls_proj_tutorial/polls/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
-def index(request) -> HttpResponse:
-    return HttpResponse(render(request, "index.html"))
+
+def index(request, id: int) -> HttpResponse:
+    data = {
+        "name": "Ranoob",
+        "disciplines": ["Dev. Web 2", "Pol. Educacionais", "Sem. Temático"],
+        "id": id,
+    }
+    return HttpResponse(render(request, "index.html", data))
