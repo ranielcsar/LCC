8854262c9d3d8cedab7849c2274f56947a2c9681
ranoob <ranielcsardev@gmail.com>
2024-09-03 09:02:20 -0300

    feat: black formatter

--- dev.web2/django/polls_proj_tutorial/polls/models.py
+++ dev.web2/django/polls_proj_tutorial/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     published_date = models.DateTimeField("date published")
@@ -12,6 +13,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- dev.web2/django/polls_proj_tutorial/polls/views.py
+++ dev.web2/django/polls_proj_tutorial/polls/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index!@#!@#!@#!")
+    return HttpResponse(render(request, "index.html"))
